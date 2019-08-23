225a1e1b890b77b2c68687636bac1e8bbd5f2ec7
yaz0r <363511+yaz0r@users.noreply.github.com>
2019-08-23 00:31:27 -0700

    More work on streaming

--- AzelLib/town/e006/twn_e006.cpp
+++ AzelLib/town/e006/twn_e006.cpp
@@ -69,52 +69,208 @@ s32 scriptFunction_605bb24(s32 r4, s32 r5)
     return 0;
 }
 
-struct sTownCutscene_28
+struct sStreamingFile_28
 {
     s32 m0;
     std::array<s32, 2> m98;
 };
 
-struct sTownCutscene
+struct GfsHn
 {
-    sTownCutscene_28 m28;
+    FILE* fHandle;
+};
+
+struct sStreamingFile_198
+{
+    GfsHn m0_gfsHandle;
+    u32 m4_transfertSectorSize;
+    u32 m8;
+    u32 mC;
+    u32 m10;
+    u32 m14_numSectors;
+    u32 m18_fileSize;
+    //size 0x20?
+};
+
+struct sStreamingFile
+{
+    sStreamingFile_28 m28;
+    u32 m4C;
+    u32 m50;
+    u32 m54;
+    u32 m58;
+    u32 m5C;
+    sStreamingFile_198 m198;
+    //size 0x1B8?
 };
 
 struct sStreamingParams
 {
-    s32* m0;
-    s32 m4;
+    sStreamingFile* m0_streamingFile;
+    s32 m4_streamingFileSize;
     u8* m8_buffer;
     s32 mC_bufferSize;
     u8* m10_audioBuffer;
     s32 m14_audioBufferSize;
 };
 
-sTownCutscene* openFileForStreaming(sStreamingParams* r4, const std::string& fileName)
+GfsHn GFS_Open(const std::string& name)
 {
-    return nullptr;
+    GfsHn temp;
+    temp.fHandle = fopen(name.c_str());
+    return temp;
+}
+
+void GFS_GetFileSize(GfsHn hanfle, s32* sectorSize, s32* numSector, s32* lastSize)
+{
+    FunctionUnimplemented();
+}
+
+sStreamingFile* initStreamingHandle(sStreamingFile* param_1, u8* buffer, u32 bufferSize, u8* audioBuffer, u32 audioBufferSize)
+{
+    FunctionUnimplemented();
+    return param_1;
+}
+
+void GFS_SetTransPara(GfsHn hanfle, s32 numSectors)
+{
+    FunctionUnimplemented();
+}
+
+void prereadStreamingData(sStreamingFile*)
+{
+    FunctionUnimplemented();
+}
+
+sStreamingFile* openFileForStreaming(sStreamingParams* param_1, const std::string& param_2)
+
+{
+    GfsHn dVar1;
+    sStreamingFile* psVar2;
+    s32 numSectors;
+    s32 sectorSize;
+    s32 lastSize;
+    s32 local_14;
+
+    dVar1 = GFS_Open(param_2);
+    if (dVar1.fHandle == 0) {
+        return NULL;
+    }
+    else {
+        psVar2 = initStreamingHandle(param_1->m0_streamingFile, param_1->m8_buffer, param_1->mC_bufferSize, param_1->m10_audioBuffer, param_1->m14_audioBufferSize);
+        if (psVar2 != (sStreamingFile*)0x0) {
+            (psVar2->m198).m0_gfsHandle = dVar1;
+            GFS_GetFileSize(dVar1, &sectorSize, &numSectors, &lastSize);
+            (psVar2->m198).m14_numSectors = numSectors;
+            (psVar2->m198).m18_fileSize = (numSectors - 1) * sectorSize + (lastSize & ~3);
+            (psVar2->m198).m8 = 0;
+            (psVar2->m198).m4_transfertSectorSize = 0x14;
+            GFS_SetTransPara(dVar1, 0x14);
+            prereadStreamingData(psVar2);
+            psVar2->m28.m0 = 2;
+        }
+    }
+    return psVar2;
 }
 
 s32 e006Task0Var0 = 0;
 
-void updateCutsceneStreaming(sTownCutscene* r4)
+void getNextStreamingBuffer(sStreamingFile* iParm1, void** pBuffer, int* pBufferSize)
+{
+    s32 iVar1;
+    s32 iVar2;
+
+    if ((iParm1->m5C == iParm1->m54) && (iParm1->m4C != iParm1->m50)) {
+        *pBufferSize = 0;
+        *pBuffer = (void*)0x0;
+        return;
+    }
+    iVar2 = iParm1->m58 - iParm1->m54;
+    if (((u32)iParm1->m54 < (u32)iParm1->m5C) && (iVar1 = iParm1->m5C - iParm1->m54, iVar1 < iVar2))
+    {
+        iVar2 = iVar1;
+    }
+    *pBufferSize = iVar2;
+    *pBuffer = (void*)iParm1->m54;
+    return;
+}
+
+void updateStreamingFileRead(sStreamingFile* iParm1)
+{
+    s32 iVar1;
+    s32 nSectors;
+    u32 uVar3;
+    sStreamingFile_198* psVar4;
+    s32 bufferSize;
+    void* buffer;
+
+    psVar4 = &iParm1->m198;
+    if ((iParm1->m198).m18_fileSize <= iParm1->m4C) {
+        return;
+    }
+    if (((iParm1->m198).m8 == 0) && (getNextStreamingBuffer(iParm1, &buffer, &bufferSize), 0x7ff < bufferSize)) {
+        if (bufferSize < (iParm1->m198).m4_transfertSectorSize * 0x800) {
+            nSectors = performDivision(0x800, bufferSize);
+        }
+        else {
+            nSectors = (iParm1->m198).m4_transfertSectorSize;
+        }
+        iVar1 = GFS_NwFread(psVar4->m0_gfsHandle, nSectors, buffer, nSectors * 0x800);
+        if (-1 < iVar1) {
+            (iParm1->m198).mC = nSectors * 0x800;
+            (iParm1->m198).m10 = 0;
+            (iParm1->m198).m8 = 1;
+        }
+        else
+        {
+            updateStreamingFileReadSub0(iParm1);
+            return;
+        }
+        
+    }
+
+    if ((iParm1->m198).m8 == 1) {
+        nSectors = GFS_NwExecOne(psVar4->m0_gfsHandle);
+        if (nSectors < 0)
+        {
+            updateStreamingFileReadSub0(iParm1);
+            return;
+        }
+
+        s32 stat;
+        s32 nbyte;
+        GFS_NwGetStat(psVar4->m0_gfsHandle, &stat, &nbyte);
+        if ((0 < nbyte) && (uVar3 = nbyte - (iParm1->m198).m10 & ~3, 0 < (int)uVar3)) {
+            (iParm1->m198).m10 = (iParm1->m198).m10 + uVar3;
+            updateStreamingFileReadSub1(iParm1, uVar3);
+        }
+        if (((iParm1->m198).mC <= nbyte) || (nSectors == 0)) {
+            (iParm1->m198).m8 = 0;
+        }
+    }
+    updateStreamingFileReadSub2(iParm1);
+}
+
+void updateCutsceneStreaming(sStreamingFile* r4)
+{
+    updateStreamingFileRead(r4);
+
+    FunctionUnimplemented();
+}
+
+void pauseCutscene(sStreamingFile* r4, s32 r5)
 {
     FunctionUnimplemented();
 }
 
-void pauseCutscene(sTownCutscene* r4, s32 r5)
-{
-    FunctionUnimplemented();
-}
-
-void cutsceneUpdateInputSub0(sTownCutscene* r4, s32 r5, s32 r6)
+void cutsceneUpdateInputSub0(sStreamingFile* r4, s32 r5, s32 r6)
 {
     r4->m28.m98[0] = r5;
     r4->m28.m98[1] = r6;
 }
 
 // TODO: kernel
-s32 scriptFunction_605861eSub0Sub0(sTownCutscene* r4)
+s32 scriptFunction_605861eSub0Sub0(sStreamingFile* r4)
 {
     return r4->m28.m0;
 }
@@ -129,12 +285,12 @@ struct sE006Task0 : public s_workAreaTemplateWithArg<sE006Task0, s32>
 
     static void Init(sE006Task0* pThis, s32 arg)
     {
-        pThis->m75 = dramAllocate(0x20000);
+        pThis->m1D4 = dramAllocate(0x20000);
 
         sStreamingParams streamingParams;
-        streamingParams.m0 = &pThis->m4;
-        streamingParams.m4 = 0x1B8;
-        streamingParams.m8_buffer = pThis->m75;
+        streamingParams.m0_streamingFile = &pThis->m4;
+        streamingParams.m4_streamingFileSize = sizeof(pThis->m4);
+        streamingParams.m8_buffer = pThis->m1D4;
         streamingParams.mC_bufferSize = 0x20000;
         streamingParams.m10_audioBuffer = nullptr;//TODO: figure this out
         streamingParams.m14_audioBufferSize = 0x8000;
@@ -219,9 +375,9 @@ struct sE006Task0 : public s_workAreaTemplateWithArg<sE006Task0, s32>
         FunctionUnimplemented();
     }
 
-    sTownCutscene* m0;
-    s32 m4;
-    u8* m75;
+    sStreamingFile* m0;
+    sStreamingFile m4;
+    u8* m1D4;
     // size: 0x1d8
 };
 
