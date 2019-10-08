4a854479aea91b50d0d3f46da56b9575df86e224
yaz0r <363511+yaz0r@users.noreply.github.com>
2019-10-07 18:22:36 -0700

    Merge macOs into main xcode solution

--- build/ios.sh
+++ build/ios.sh
@@ -2,7 +2,9 @@ rm -rf ios
 mkdir ios 
 rm -rf tvos
 mkdir tvos
-
+rm -rf osx
+mkdir osx
 (cd ios && cmake -G Xcode ../.. -DCMAKE_SYSTEM_NAME=iOS)
 (cd tvos && cmake -G Xcode ../.. -DCMAKE_SYSTEM_NAME=tvOS)
+(cd osx && cmake -G Xcode ../.. -DCMAKE_SYSTEM_NAME=Darwin)
 
